274b3f3f0203b031b271ef151d2fc45c8140c71a
Dave Compton <davecompton7@gmail.com>
2017-05-12 19:38:53 +0000

    made line dynamic

--- Main.hs
+++ Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 import Reflex
 import Reflex.Dom
+import Reflex.Dom.Time
 import Data.Map as DM (Map, fromList)
 import Data.Text (Text, pack)
 import Data.List (transpose)
+import Data.Time.Clock (getCurrentTime)
+import Control.Monad.Trans (liftIO)
 
 main :: IO ()
 main = mainWidget $ do 
@@ -19,17 +23,20 @@ showBoard = do
 
 showLine :: MonadWidget t m => m ()
 showLine = do
-  let lineAttrs = 
+  now <- liftIO getCurrentTime 
+  ticks <- tickLossy 0.01 now
+  counter <- foldDyn (\_ c -> c+1) (0::Int) ticks
+
+  let lineAttrs count = 
          fromList [ ("x1", "10")
                   , ("y1", "10")
-                  , ("x2", "200")
+                  , ("x2", pack $ show count)
                   , ("y2", "10")
                   , ("stroke-width", "2")
                   , ("stroke", "black")
                   ]
-  elSvgns "line" (constDyn $ lineAttrs) $ return ()
 
-
+  elSvgns "line"  (fmap lineAttrs counter) $ return ()
 
 -- Wrapper around elDynAttrNS'
 elSvgns :: MonadWidget t m => Text -> Dynamic t (Map Text Text) -> m a -> m a
