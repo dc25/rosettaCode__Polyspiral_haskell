1cc833d05c2d3066b0a8e4dbf5c6faf6ffa21566
Dave Compton <davecompton7@gmail.com>
2017-05-12 22:19:01 +0000

    cleanup, indentation

--- Main.hs
+++ Main.hs
@@ -7,59 +7,63 @@ import Data.Map (Map, fromList)
 import Data.Time.Clock (getCurrentTime)
 import Control.Monad.Trans (liftIO)
 
-width = 600
-height = 500
+width = 800
+height = 800
 
 type Point = (Float,Float)
 type Segment = (Point,Point)
 
 main = mainWidget $ do 
+
   let boardAttrs = 
          fromList [ ("width" , pack $ show width)
                   , ("height", pack $ show height)
                   ]
 
   now <- liftIO getCurrentTime 
-  dTicks <- tickLossy 0.1 now
+  dTicks <- tickLossy 0.05 now
   dCounter <- foldDyn (\_ c -> c+1) (0::Int) dTicks
-  let dAngle =fmap (\c -> fromIntegral c / 800.0) dCounter
-      dLines = fmap toLineMap dAngle
 
-  el "div" $ elSvgns "svg" (constDyn boardAttrs) $ listWithKey dLines showLine
+  let dAngle = fmap (\c -> fromIntegral c / 800.0) dCounter
+
+  el "div" 
+    $ elSvgns "svg" (constDyn boardAttrs) 
+    $ listWithKey (fmap toLineMap dAngle) showLine
+
   return ()
 
+showLine :: MonadWidget t m => Int -> Dynamic t Segment -> m ()
+showLine _ dSegment = elSvgns "line" (lineAttrs <$> dSegment) $ return ()
+
+advance :: Float -> (Point, Float, Float) -> (Point, Float, Float)
 advance angle ((x,y), len, rot) = 
   let new_x = x + len * cos rot
       new_y = y + len * sin rot
-      new_len = len + 5.0 
+      new_len = len + 3.0 
       new_rot = rot + angle
   in ((new_x, new_y), new_len, new_rot)
 
+segments :: [Point] -> [Segment]
 segments  pts  =  zip pts $ tail pts
 
 toLineMap :: Float -> Map Int ((Float,Float),(Float,Float))
 toLineMap angle =
-         fromList   -- changes list to map (for listWithKey)
-       $ zip [0..]  -- annotates segments with index
-       $ segments  -- changes points to line segments
-       $ take 40    -- limit the number of points
-       $ (\(pt,_,_) -> pt)  -- cull out the (x,y) values
-     <$> iterate (advance angle) ((0.0, 0.0), 0.0, 0.0) 
+      fromList   -- changes list to map (for listWithKey)
+  $   zip [0..]  -- annotates segments with index
+  $   segments   -- changes points to line segments
+  $   take 40    -- limit the number of points
+  $   (\(pt,_,_) -> pt)  -- cull out the (x,y) values
+  <$> iterate (advance angle) ((0.0, 0.0), 0.0, 0.0) 
 
 lineAttrs :: Segment -> Map Text Text
 lineAttrs ((x1,y1), (x2,y2)) =
-    fromList [ ( "x1",    pack $ show (width/2+x1))
-             , ( "y1",    pack $ show (height/2+y1))
-             , ( "x2",    pack $ show (width/2+x2))
-             , ( "y2",    pack $ show (height/2+y2))
-             , ( "style", "stroke:red;stroke-width:2")
-             ]    
+  fromList [ ( "x1",    pack $ show (width/2+x1))
+           , ( "y1",    pack $ show (height/2+y1))
+           , ( "x2",    pack $ show (width/2+x2))
+           , ( "y2",    pack $ show (height/2+y2))
+           , ( "style", "stroke:red;stroke-width:2")
+           ]    
          
-showLine :: MonadWidget t m => Int -> Dynamic t Segment -> m ()
-showLine _ dSegment = do
-    elSvgns "line" (lineAttrs <$> dSegment) $ return ()
-    return ()
-
 -- Wrapper around elDynAttrNS'
 elSvgns :: MonadWidget t m => Text -> Dynamic t (Map Text Text) -> m a -> m a
 elSvgns t m ma = do
